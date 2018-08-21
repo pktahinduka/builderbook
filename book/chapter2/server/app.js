e1901ac68219fbf7f451526c8264477d74786cd1
Ichama Madrara Herbert <petertahinduka@gmail.com>
2018-08-22 00:24:05 +0300

    Functionality to execute connection to MongoDB

---
+++ book/chapter2/server/app.js
@@ -0,0 +1,62 @@
+/* eslint-disable */
+
+import dotenv from 'dotenv';
+
+import express from 'express';
+import next from 'next';
+
+import mongoose from 'mongoose';
+
+import User from './models/User';
+
+import session from 'express-session';
+import mongoSessionStore from 'connect-mongo';
+
+dotenv.config();
+
+const dev = process.env.NODE_ENV !== 'production';
+const MONGO_URL = process.env.MONGO_URL_TEST;
+
+mongoose.connect(MONGO_URL, { useNewUrlParser: true });
+
+const port = process.env.PORT || 8000;
+const ROOT_URL = process.env.ROOT_URL || `http://localhost:${port}`;
+
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  const MongoStore = mongoSessionStore(session);
+
+  const sess = {
+    name: 'buiderbook.sid',
+    secret: 'HD2w.)q*VqRT4/#NK2M/,E^B)}FED5fWU!dKe[wk',
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 14 * 24 * 60 * 60, //save session for 14 days
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+    },
+  };
+
+  server.use(session(sess));
+
+  server.get('/', async (req, res) => {
+    req.session.foo = 'bar';
+    const user = await User.findOne({ slug: 'team-builder-book' });
+    app.render(req, res, '/', { user });
+  });
+
+  server.get('*', (req, res) => handle(req, res));
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on ${ROOT_URL}`); // eslint-disable-line no-console
+  });
+});
